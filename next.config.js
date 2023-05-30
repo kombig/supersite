985839e94b2280cebe3c973459a93f8c28c5c059
Andrey <71153151+kombig@users.noreply.github.com>
2023-05-30 20:27:25 +0300

    site

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     loader: 'imgix',
-    path: 'http://localhost:3000/'
+      path: 'https://nextjs-o7qg.vercel.app/'
   },
   exportPathMap: async function () {
     return {
